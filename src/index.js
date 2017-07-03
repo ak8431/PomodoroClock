f0047f642ab77fe35aa4c848e7602e0d289d5bc5
Amit Kumar <amitkumarmaurya8431@gmail.com>
2017-07-03 11:04:51 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './App.css';
 // import App from './App';
-// import App from './Pomodoro';
+import App from './Pomodoro';
 // import Clock from './clock'
-import TestClock from './try2'
+// import TestClock from './try2'
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<TestClock />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
